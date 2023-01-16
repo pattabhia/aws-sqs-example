ad0951748cbd3951a327e245b782d7dd1e79ae1d
pattabhia <shivaram2004@gmail.com>
2023-01-16 13:52:08 +0530

    first commit

---
+++ src/test/java/com/devcodeworld/aws/sqs/AwsSqsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcodeworld.aws.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSqsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
